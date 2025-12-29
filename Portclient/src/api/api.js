857367fac953cd148a6ba158d66461ac29a87d67
Mathesh-299 <postbox2992005@gmail.com>
2025-12-29 09:50:06 +0530

    Updates the API endpoint

--- Portclient/src/api/api.js
+++ Portclient/src/api/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "https://portfolio-server-a9ot.onrender.com/api",
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://portfolio-server-a9ot.onrender.com/api",
+    // baseURL: "http://localhost:8000/api",
 });
 
 export default API;
